da84a14ae1c2010c61afb134b1526eb3182e2fb7
juliacereja <jcerejameinhardt@gmail.com>
2026-04-30 16:23:00 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -13,11 +13,19 @@ st.markdown("<h1 style='text-align: center;'>Antes de partir eu quero...</h1>", 
 if "tarefas" not in st.session_state:
     st.session_state.tarefas = []
 
-nova_tarefa = st.text_input("Nova tarefa")
+if "input_tarefa" not in st.session_state:
+    st.session_state.input_tarefa = ""
+
+nova_tarefa = st.text_input("Nova tarefa", key="input_tarefa")
 
 if st.button("Adicionar"):
-    if nova_tarefa:
-        st.session_state.tarefas.append({"texto": nova_tarefa, "feito": False})
+    if st.session_state.input_tarefa:
+        st.session_state.tarefas.append({
+            "texto": st.session_state.input_tarefa,
+            "feito": False
+        })
+      
+        st.session_state.input_tarefa = ""
 
 st.divider()
 
@@ -34,4 +42,5 @@ for i, tarefa in enumerate(st.session_state.tarefas):
     with col2:
         if st.button("❌", key=f"del_{i}"):
             st.session_state.tarefas.pop(i)
+            st.rerun()
             st.rerun()
