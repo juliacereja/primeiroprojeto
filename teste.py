57b8df80b733f194f359c7ede95a37c35ffc25a8
juliacereja <jcerejameinhardt@gmail.com>
2026-05-15 10:15:00 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -81,7 +81,7 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 def gerar_ideia():
 
-    prompt = "Crie uma ideia, resumida a 2 ou 3 palavras, de meta para uma lista de coisas para fazer antes de morrer."
+    prompt = "Crie uma ideia, resumida a 2 ou 3 palavras, (que não esteja entre essas: pular de paraquedas, ler 20 livros, viajar de avião, visitar 5 cidades do Brasil, ter filhos, amar de verdade, fazer uma aula de dança, realizar um sonho com alguém, viajar para o exterior, cozinhar uma comida só para você, partivipar de um campeonato, ir ao show de um cantor que você ama), de meta para uma lista de coisas para fazer antes de morrer."
 
     resposta = model.generate_content(prompt)
 
